e582488b013b6aa0b0db2d5b6a062a884463310a
dongdong-byte <kimeee34@gmail.com>
2025-12-03 17:30:56 +0900

    도커 파일 백업

---
+++ projectDockerDB/src/test/java/kim/projectdockerdb/ProjectDockerDbApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.projectdockerdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDockerDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
